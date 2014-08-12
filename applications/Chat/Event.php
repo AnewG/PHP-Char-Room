1c20a43f2d49f4b7aff27fcedc56921d8f39d27d
anewg <breakitwall@gmail.com>
2014-08-12 13:34:45 +0800

    add redbean orm library

--- applications/Chat/Event.php
+++ applications/Chat/Event.php
@@ -6,7 +6,14 @@
  * @author walkor <worker-man@qq.com>
  * 
  */
+
+/* @TODO 永久保存内容，往上翻页获取，私信，系统广播，搜索聊天记录 */
+
+/* Table - user & message */
+
 require_once ROOT_DIR . '/Protocols/WebSocket.php';
+require_once ROOT_DIR . '/Lib/Rb.php';  // include RedBean ORM library
+
 class Event
 {
     /**
@@ -24,6 +31,17 @@ class Event
     */
    public static function onConnect($message)
    {
+       // init once
+       R::setup(
+         'mysql:host=localhost;dbname=char',
+         'chat',
+         'chat'
+       );
+
+       // R::selectDatabase( 'otherDbName' );
+       $_userTable = R::dispense( 'user' );
+       $_msgTable  = R::dispense( 'message' );
+
        // WebSocket 握手阶段
        if(0 === strpos($message, 'GET'))
        {
@@ -34,24 +52,43 @@ class Event
                $Sec_WebSocket_Key = $match[1];
            }
            $new_key = base64_encode(sha1($Sec_WebSocket_Key."258EAFA5-E914-47DA-95CA-C5AB0DC85B11",true));
+
            // 握手返回的数据
            $new_message = "HTTP/1.1 101 Switching Protocols\r\n";
            $new_message .= "Upgrade: websocket\r\n";
            $new_message .= "Sec-WebSocket-Version: 13\r\n";
            $new_message .= "Connection: Upgrade\r\n";
            $new_message .= "Sec-WebSocket-Accept: " . $new_key . "\r\n\r\n";
+
+           // 非法返回的数据
+           $close_message = "HTTP/1.1 403 Forbidden\r\n";
+           $close_message .= "Allow-login: no\r\n\r\n";
            
-           /*
-            * 获取uid，uid必须为1-42亿内的数字
-            * 这里作为Demo把时间戳当成uid，高并发下这里会有小概率uid冲突，开发者可以使用自己的uid获取方法
-            * 一般流程应该是通过用户名 密码从数据库中获取uid
-            * 用户名密码可以放到url中作为参数传递过来，然后自行解析
-            * 例如前端js这样调用 ws = new WebSocket("ws://workerman.net:7272/?name=xxx&password=xxx");
-            */ 
-           $uid = substr(strval(microtime(true)), 3, 10)*100;
-           
+           // 判断账密
            preg_match('#GET\s+\/\?(.*)\s+HTTP/1.1#i', $message , $match);
-           var_dump($match[1]); // name=xxx&password=xxx
+           parse_str($match[1],$user_info); // name=xxx&password=xxx
+
+           // var_dump($user_info);
+
+           if( empty($user_info['name']) || empty($user_info['password']) ){
+
+               return GateWay::sendToCurrentUid($close_message);
+
+           }else{
+
+               $result = R::getRow( 'SELECT id FROM user WHERE name = :name AND password = :password LIMIT 1',
+                   array(
+                     ':name'     => $user_info['name'],
+                     ':password' => md5($user_info['password'])
+                   )
+               );
+
+               if(is_null($result)){
+                   return GateWay::sendToCurrentUid($close_message);
+               }else{
+                   $uid = $result['id'];
+               }
+           }
 
            // 记录uid到gateway通信地址的映射
            GateWay::storeUid($uid);
@@ -61,10 +98,9 @@ class Event
            
            // 发送数据包到客户端 完成握手
            return GateWay::sendToCurrentUid($new_message);
-       }
-       // 如果是flash发来的policy请求
-       elseif(trim($message) === '<policy-file-request/>')
-       {
+
+       }elseif(trim($message) === '<policy-file-request/>'){
+           // 如果是flash发来的policy请求
            $policy_xml = '<?xml version="1.0"?><cross-domain-policy><site-control permitted-cross-domain-policies="all"/><allow-access-from domain="*" to-ports="*"/></cross-domain-policy>'."\0";
            return GateWay::sendToCurrentUid($policy_xml);
        }
@@ -86,7 +122,6 @@ class Event
 
        // 广播 xxx 退出了
        GateWay::sendToAll(\WebSocket::encode(json_encode(array('type'=>'logout', 'uid'=> $uid, 'time'=>date('Y-m-d H:i:s')))));
-       
    }
    
    /**
@@ -96,14 +131,15 @@ class Event
     */
    public static function onMessage($uid, $message)
    {
-        if(\WebSocket::isClosePacket($message))
-        {
+        // 检测是否是断开包
+        if(\WebSocket::isClosePacket($message)){
             Gateway::kickUid($uid, '');
             self::onClose($uid);
             return;
         }
+
         $message =WebSocket::decode($message);
-        echo "uid:$uid onMessage:".var_export($message,true)."\n";
+        // echo "uid:$uid onMessage:".var_export($message,true)."\n";
         $message_data = json_decode($message, true);
         if(!$message_data)
         {
--- applications/Chat/Web/index.php
+++ applications/Chat/Web/index.php
@@ -64,62 +64,67 @@
 
     var ws, name, user_list={};
 
-    function init() {
+    function init(key , value) {
+
       // 创建websocket
-    	ws = new WebSocket("ws://"+document.domain+":7272/?name=xxx&password=yyy");
-      // 当socket连接打开时，输入用户名
+    	ws = new WebSocket("ws://"+document.domain+":7272/?name="+key+"&password="+value);
+
+      // 当socket连接打开
       ws.onopen = function() {
-    	  show_prompt();
-    	  if(!name) {
-    		  return ws.close();
-   		  }
-    	  ws.send(JSON.stringify({"type":"login","name":name}));
+
+        // return ws.close(); 客户端关闭 socket
+
+        // 隐藏悬浮框
+        $('#close_fall')[0].click();
+
+    	  ws.send(JSON.stringify({"type":"login","name":key}));
+
       };
+
       // 当有消息时根据消息类型显示不同信息
       ws.onmessage = function(e) {
-    	  console.log(e.data);
+    	  // console.log(e.data);
         var data = JSON.parse(e.data);
         switch(data['type']){
-              // 展示用户列表
-              case 'user_list':
-            	  //{"type":"user_list","user_list":[{"uid":xxx,"name":"xxx"},{"uid":xxx,"name":"xxx"}]}
-            	  flush_user_list(data);
-            	  break;
-              // 登录
-              case 'login':
-                  //{"type":"login","uid":xxx,"name":"xxx","time":"xxx"}
-            	  add_user_list(data['uid'], data['name']);
-                  say(data['uid'], 'all',  data['name']+' 加入了聊天室', data['time']);
-                  break;
-              // 发言
-              case 'say':
-            	  //{"type":"say","from_uid":xxx,"to_uid":"all/uid","content":"xxx","time":"xxx"}
-            	  say(data['from_uid'], data['to_uid'], data['content'], data['time']);
-            	  break;
-             // 用户退出 
-              case 'logout':
-            	  //{"type":"logout","uid":xxx,"time":"xxx"}
-         		 say(data['uid'], 'all', user_list['_'+data['uid']]+' 退出了', data['time']);
-         		 del_user_list(data['uid']);
-        }
-      };
-      ws.onclose = function() {
-    	  console.log("服务端关闭了连接");
-      };
-      ws.onerror = function() {
-    	  console.log("出现错误");
-      };
-    }
 
-    // 输入姓名
-    function show_prompt(){  
-        name = prompt('输入你的名字：', '');
-        if(!name){  
-            alert('姓名输入为空，请重新输入！');  
-            show_prompt();
+          // 展示用户列表
+          case 'user_list':
+            //{"type":"user_list","user_list":[{"uid":xxx,"name":"xxx"},{"uid":xxx,"name":"xxx"}]}
+            flush_user_list(data);
+            break;
+
+          // 登录
+          case 'login':
+            //{"type":"login","uid":xxx,"name":"xxx","time":"xxx"}
+            add_user_list(data['uid'], data['name']);
+            say(data['uid'], 'all',  data['name']+' 加入了聊天室', data['time']);
+            break;
+
+          // 发言
+          case 'say':
+            //{"type":"say","from_uid":xxx,"to_uid":"all/uid","content":"xxx","time":"xxx"}
+            say(data['from_uid'], data['to_uid'], data['content'], data['time']);
+            break;
+
+          // 用户退出 
+          case 'logout':
+            //{"type":"logout","uid":xxx,"time":"xxx"}
+         	  say(data['uid'], 'all', user_list['_'+data['uid']]+' 退出了', data['time']);
+         	  del_user_list(data['uid']);
         }
-        //name = name.replace(/\"/g,'\\"');
-    }  
+      };
+
+      ws.onclose = function() {
+    	  // console.log("服务端关闭了连接");
+        alert('账密错误');
+      };
+
+      ws.onerror = function() {
+    	  // console.log("出现错误");
+        alert('服务器出现错误');
+      };
+
+    }
 
     // 提交对话
     function onSubmit() {
@@ -136,47 +141,48 @@
     }
 
     // 删除一个用户从用户列表
-    function del_user_list(uid)
-    {
-    	delete user_list['_'+uid];
-    	flush_user_list_window();
+    function del_user_list(uid){
+      delete user_list['_'+uid];
+      flush_user_list_window();
     }
 
     // 刷新用户列表数据
     function flush_user_list(data){
-    	user_list = {};
-    	if('user_list' in data){
-	    	for(var p in data['user_list']){
-	   	 	    user_list['_'+data['user_list'][p]['uid']] = data['user_list'][p]['name'];
-	   		}
-        }
-    	flush_user_list_window();
+      user_list = {};
+      if('user_list' in data){
+	      for(var p in data['user_list']){
+	        user_list['_'+data['user_list'][p]['uid']] = data['user_list'][p]['name'];
+	      }
+      }
+      flush_user_list_window();
     }
 
     // 刷新用户列表框
     function flush_user_list_window(){
-    	var userlist_window = document.getElementById("userlist");
-    	userlist_window.innerHTML = '<h4>在线用户</h4><ul>';
-    	for(var p in user_list){
-    		userlist_window.innerHTML += '<li id="'+p+'">'+user_list[p]+'</li>';
+      var userlist_window = document.getElementById("userlist");
+      userlist_window.innerHTML = '<h4>在线用户</h4><ul>';
+      for(var p in user_list){
+        userlist_window.innerHTML += '<li id="'+p+'">'+user_list[p]+'</li>';
       }
-    	userlist_window.innerHTML += '</ul>';
+      userlist_window.innerHTML += '</ul>';
     }
 
     // 发言
     function say(from_uid, to_uid, content, time){
-    	var dialog_window = document.getElementById("dialog"); 
+      var dialog_window = document.getElementById("dialog"); 
     	switch(to_uid){
-    		   case 'all':
-    			   if(user_list['_'+from_uid]){
-    				   dialog_window.innerHTML +=  '<div class="speech_item"><img src="http://lorempixel.com/38/38/?'+from_uid+'" class="user_icon" /> '+user_list['_'+from_uid]+' <br> '+time+'<div style="clear:both;"></div><p class="triangle-isosceles top">'+content+'</p> </div>';
-    			   }
-    			   break;
-    		   // 私聊
-    		   default :
-    			   if(user_list['_'+from_uid]){
-    				   dialog_window.innerHTML +=  '<div class="speech_item"><img src="http://lorempixel.com/38/38/?'+from_uid+'" class="user_icon" /> '+user_list['_'+from_uid]+' <br> '+time+'<div style="clear:both;"></div><p class="triangle-isosceles top">'+content+'</p> </div>';
-   				   }
+    	  case 'all':
+    		  if(user_list['_'+from_uid]){
+    			  dialog_window.innerHTML +=  '<div class="speech_item"><img src="http://lorempixel.com/38/38/?'+from_uid+'" class="user_icon" /> '+user_list['_'+from_uid]+' <br> '+time+'<div style="clear:both;"></div><p class="triangle-isosceles top">'+content+'</p> </div>';
+    			}
+    			break;
+
+    		// 私聊
+    		default :
+    			if(user_list['_'+from_uid]){
+    			  dialog_window.innerHTML +=  '<div class="speech_item"><img src="http://lorempixel.com/38/38/?'+from_uid+'" class="user_icon" /> '+user_list['_'+from_uid]+' <br> '+time+'<div style="clear:both;"></div><p class="triangle-isosceles top">'+content+'</p> </div>';
+   				}
+          break;
     	}
     }
 
@@ -186,6 +192,27 @@
 
       $('#close_page').on('click',function(){
         location.href = "about:blank";
+      });
+
+      $('#sign_in').on('click',function(){
+
+        var k = $('#user_name').val();
+        var v = $('#pass_word').val();
+
+        if(k == ''){
+          $('#user_name').parent().addClass('has-error');
+          $('#user_name').focus();
+          return false;
+        }
+
+        if(v == ''){
+          $('#pass_word').parent().addClass('has-error');
+          $('#pass_word').focus();
+          return false;
+        }
+
+        init(k , v);
+
       });
     });
   </script>
@@ -201,22 +228,29 @@
 
       <div class="col-md-6 column">
 
+         <button style="width:100%;">更早</button>
+
          <div class="thumbnail">
            <!-- talk zone -->
            <div class="caption" id="dialog"></div>
+
+           <button style="width:100%;">搜索</button>
          </div>
 
          <form onsubmit="onSubmit(); return false;">
            <!-- commit zone -->
            <textarea class="textarea thumbnail" id="textarea"></textarea>
            <div class="say-btn">
-             <input type="submit" class="btn btn-default" value="发表" />
+             <input type="submit" class="btn btn-success" value="发表" />
            </div>
          </form>
 
       </div>
 
       <div class="col-md-3 column">
+
+        <div style="margin-top:25px;"></div>
+
         <div class="thumbnail">
           <!-- user_list -->
           <div class="caption" id="userlist"></div>
@@ -235,22 +269,23 @@
       <div class="form-group">
         <div class="input-group">
           <div class="input-group-addon">@</div>
-          <input class="form-control" type="text" placeholder="User Name">
+          <input id="user_name" class="form-control" type="text" placeholder="User Name">
         </div>
       </div>
 
       <div class="form-group">
         <label class="sr-only" for="exampleInputPassword2">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+        <input id="pass_word" type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
       </div>
 
       <button style="margin-left:10px;" id="close_page" class="pull-right btn btn-default">Close</button>
-      <button type="submit" class="pull-right btn btn-success">Sign in</button>
+      <button type="button" id="sign_in" class="pull-right btn btn-success">Sign in</button>
+
       <button class="fall_close" id="close_fall" style="display:none;">CloseFall</button>
+      <button id="open_fall" class="fall_open btn btn-default" style="display:none;">OpenFall</button>
 
   </div>
 </div>
-<button id="open_fall" class="fall_open btn btn-default" style="display:none;">OpenFall</button>
 <!-- ************ 登录区 End ************ -->
 
 </body>
